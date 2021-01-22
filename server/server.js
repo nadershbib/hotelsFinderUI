744c8436d2ae8edf4d87b9b437da30527256b51d
Nader Shbib <nader_shbib@hotmail.com>
2021-01-21 20:29:36 -0800

    added

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3001;
 app.use(express.static(publicPath));
+console.log("🚀 ~ file: server.js ~ line 8 ~ publicPath", publicPath)
+console.log("🚀 ~ file: server.js ~ line 9 ~ __dirname", __dirname)
 app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
 });
